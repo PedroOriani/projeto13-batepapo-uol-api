40e9594f681ef8c65b0ebed0b7fc234bd900b62c
Pedro Oriani <pedro.oriani@gmail.com>
2023-07-02 23:40:03 -0300

    connect

---
+++ src/app.js
@@ -0,0 +1,42 @@
+import express  from "express";
+import { MongoClient } from "mongodb";
+import cors from "cors";
+import dayjs from "dayjs";
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+let db;
+
+mongoClient.connect()
+    .then(() => db = mongoClient.db())
+    .catch((err) => console.log(err.message))
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Servidor esta rodando na porta ${PORT}`));
+
+app.post('/participants', async (req, res) => {
+
+})
+
+app.get('/participants', async (req, res) => {
+    const participantsList = await db.collection('participants').find().toArray()
+    return res.send(participantsList)
+})
+
+app.post('/messages', async (req, res) => {
+
+})
+
+app.get('/messages', async (req, res) => {
+    
+})
+
+app.post('/status', async (req, res) => {
+
+})
